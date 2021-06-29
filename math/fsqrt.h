d159713657f45ff14ba7bcfe57afc635f0dcb921
grytole <grytole@gmail.com>
2021-06-29 19:13:01 +0300

    add isqrt() and fsqrt() implementations

---
+++ math/fsqrt.h
@@ -0,0 +1,6 @@
+#ifndef __MATH_FSQRT_H__
+#define __MATH_FSQRT_H__
+
+float fsqrt(float x);
+
+#endif /* __MATH_FSQRT_H__ */
---
+++ math/isqrt.h
@@ -0,0 +1,8 @@
+#ifndef __MATH_ISQRT_H__
+#define __MATH_ISQRT_H__
+
+#include <stdint.h>
+
+uint32_t isqrt(uint32_t x);
+
+#endif /* __MATH_ISQRT_H__ */
